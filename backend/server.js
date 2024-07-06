44c1c0093bcfd3ea4a922a501a44130da47fdd53
SjxSubham <sjxsubham@gmail.com>
2024-07-07 04:04:49 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,16 @@ import userRoutes from './routes/user.route.js'
 import exploreRoutes from './routes/explore.route.js'
 import authRoutes from './routes/auth.route.js'
 import connectMongoDB from './db/connectMongoDB.js';
+import path from 'path';
 
 dotenv.config();
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
+console.log("dirname", __dirname);
 
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -21,15 +25,17 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
-app.get("/", (req,res) => {
-    res.send("Server is ready");
-});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-    console.log("Server started on http://localhost:5000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=> {
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+})
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
     connectMongoDB();
 });
