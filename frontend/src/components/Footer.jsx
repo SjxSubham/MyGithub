0222985c5bf0fe926427d3d1bb7f9364958d9f62
SjxSubham <sjxsubham@gmail.com>
2024-07-17 01:00:33 +0530

    changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='py-2 px-3 position: fixed md:absolute bottom-4 right-4 hover:p x-4 rounded-md hover:scale-105 transition-all hover:-rotate-1 border shadow-yellow-600'>Created by{" "} <a>Sjx</a></div>
+    <div className='py-2 px-3 position: relative md:absolute bottom-4 right-4 hover:p x-4 rounded-md hover:scale-105 transition-all hover:-rotate-1 border shadow-yellow-600'>Created by{" "} <a>Sjx</a></div>
   )
 }
 
