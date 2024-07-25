ea8789d09f0b92c3d2a2673e5ae6abb7cadcf356
SjxSubham <sjxsubham@gmail.com>
2024-07-26 01:17:52 +0530

    hmn

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Graph = ({ userProfile }) => {
   return (
     <div>
-      <a href={`https://github.com/ashutosh00710/github-readme-activity-graph`}>
+      <a >
         <img 
           src={`https://github-readme-activity-graph.vercel.app/graph?username=${userProfile?.name}&theme=github-compact&bg_color=282C35`} 
           alt={`${userProfile?.name} GitHub activity graph`} 
         />
       </a>
     </div>
+    // 
   );
 };
 
