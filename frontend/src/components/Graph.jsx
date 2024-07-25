9b639f5b414f22d93c1e9fc914492b6a9ce6ebbf
SjxSubham <sjxsubham@gmail.com>
2024-07-26 01:23:27 +0530

    update html_url

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -5,8 +5,8 @@ const Graph = ({ userProfile }) => {
     <div>
       <a >
         <img 
-          src={`https://github-readme-activity-graph.vercel.app/graph?username=${userProfile?.name}&theme=github-compact&bg_color=282C35`} 
-          alt={`${userProfile?.name} GitHub activity graph`} 
+          src={`https://github-readme-activity-graph.vercel.app/graph?username=${userProfile?.html_url}&theme=github-compact&bg_color=282C35`} 
+          alt={`${userProfile?.html_url} GitHub activity graph`} 
         />
       </a>
     </div>
