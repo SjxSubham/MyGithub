c810a7d1631f16e930023f6e1bdbbd20cc5d3f4f
SjxSubham <sjxsubham@gmail.com>
2024-07-26 01:11:57 +0530

    changes

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Graph = () => {
+const Graph = ({ userProfile }) => {
   return (
-    <div >
-        [![{userProfile?.name} GitHub activity graph](https://github-readme-activity-graph.vercel.app/graph?username={userProfile?.name}&theme=github-compact&bg_color=282C35)](https://github.com/ashutosh00710/github-readme-activity-graph)
+    <div>
+      <a href={`https://github.com/ashutosh00710/github-readme-activity-graph`}>
+        <img 
+          src={`https://github-readme-activity-graph.vercel.app/graph?username=${userProfile?.name}&theme=github-compact&bg_color=282C35`} 
+          alt={`${userProfile?.name} GitHub activity graph`} 
+        />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Graph
+export default Graph;
