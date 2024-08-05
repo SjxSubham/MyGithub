fe04837f86b080aff5cd42fb1096eb3c1da8b3e3
SjxSubham <sjxsubham@gmail.com>
2024-08-05 20:31:34 +0530

    Footer Sticked beautiflly

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     
-    <div className='bg-glass py-2 px-3 fixed md:absolute bottom-4 right-4 hover:p x-4 rounded-md hover:scale-105 transition-all hover:-rotate-1 border shadow-yellow-600'>Created by{" "} <a>Sjx</a></div>
+    <div className='bg-glass py-2 px-3 fixed bottom-4 right-4 hover:p x-4 rounded-md hover:scale-105 transition-all hover:-rotate-1 border shadow-yellow-600'>Created by{" "} <a>Sjx</a></div>
   )
 }
 
