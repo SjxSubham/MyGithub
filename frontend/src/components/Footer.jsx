456664a72e1f947747c5ac456de750247021b0ef
SjxSubham <sjxsubham@gmail.com>
2024-10-28 00:46:27 +0530

    test

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,5 +6,6 @@ const Footer = () => {
     <div className='bg-glass py-2 px-3 fixed bottom-4 right-4 hover:p x-4 rounded-md hover:scale-105 transition-all hover:-rotate-1 border shadow-yellow-600'>Created by{" "} <a>Sjx</a></div>
   )
 }
+//added footer
 
 export default Footer
